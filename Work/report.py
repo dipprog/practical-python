beeeb226497325b90b7da77e27a2f9ead7d45ee7
Dip Kumar Das <dipkumardasdh@gmail.com>
2023-08-05 16:02:34 +0530

    added and modified some files

--- Work/report.py
+++ Work/report.py
@@ -1,20 +1,22 @@
 # report.py
 
 import fileparse
-import stock
+from stock import Stock
 import tableformat
+from portfolio import Portfolio
 
 
-def read_portfolio(filename):
+def read_portfolio(filename, **opts):
     '''
     Read a stock portfolio file into a list of dictionaries with keys
     name, shares, and price.
     '''
     with open(filename) as lines:
         portdicts = fileparse.parse_csv(
-            lines, select=['name', 'shares', 'price'], types=[str, int, float])
+            lines, select=['name', 'shares', 'price'], types=[str, int, float], **opts)
 
-        return [stock.Stock(d['name'], d['shares'], d['price']) for d in portdicts]
+    portfolio =  [ Stock(**d) for d in portdicts ]
+    return Portfolio(portfolio)
 
 
 def read_prices(filename):
